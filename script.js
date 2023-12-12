50204d285f017bf2e8ae6841e705db1785db8e3d
josefinek <josefine@kafer.com>
2023-12-12 16:30:51 +0100

    I still don't understand the assignment.

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
 export function exercise01(args) /*defining the function*/ {
   const input = args
   const result = []
- 
+
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
     if (currentElement === "e" || currentElement === "E") {
@@ -13,25 +12,24 @@ export function exercise01(args) /*defining the function*/ {
   }
   return result.join("")
 }
- 
+
 export function exercise02(args) {
   return args.toUpperCase()
 }
 
 export function exercise03(args) {
-const input= "Two Words"
-const result = []
-let count = 0
+  const input = args
+  const result = []
+  let count = 0
 
-for(let i = 0; i< input.length; i++ ) {
-const currentElement = input [i]
-if (currentElement === 'e'){
-count = count +1
-}
-else if ( currentElement=== 'E'){
-count = count +1
-}
-}
-return count +1 
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "e") {
+      count = count + 1
+    } else if (currentElement === "E") {
+      count = count + 1
+    }
+  }
+
 }
 
